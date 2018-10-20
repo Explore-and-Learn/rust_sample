d35e45f34808499b17d9b564f893c30c54970db0
marty-hope <marty.hope@gmail.com>
2018-10-20 10:00:43 -0700

    Update sample to include fibonnaci function

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", get_fibonnaci_value_by_index(10));
+}
+
+fn get_fibonnaci_value_by_index(i : i32) -> i32 {
+    if i == 0 {
+        return 1;
+    }   
+    if i == 1 {
+        return 1;
+    }
+    return get_fibonnaci_value_by_index(i - 1) + get_fibonnaci_value_by_index(i - 2);
 }
