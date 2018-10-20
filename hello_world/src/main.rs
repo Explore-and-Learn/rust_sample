71e4ffc8827b8d9acbe3f9ade6f5c64fae8c626e
marty-hope <marty.hope@gmail.com>
2018-10-20 15:58:44 -0700

    Minor refactor

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -4,12 +4,9 @@ fn main() {
 }
 //This is a comment
 fn get_fibonnaci_value_by_index(i : i32) -> i32 {
-    if i == 0 {
+    if i == 0 || i == 1 {
         return 1;
-    }   
-    if i == 1 {
-        return 1;
-    }
+    } 
     return get_fibonnaci_value_by_index(i - 1) + get_fibonnaci_value_by_index(i - 2);
 }
 
