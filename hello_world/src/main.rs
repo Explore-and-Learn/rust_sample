8fdcadf70e97188a62edd7c602a62452bb73ce2a
marty-hope <marty.hope@gmail.com>
2018-10-20 12:22:46 -0700

    Add trivial get_is_even function

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("{}", get_fibonnaci_value_by_index(10));
+    println!("{}", get_is_even(101));
 }
 //This is a comment
 fn get_fibonnaci_value_by_index(i : i32) -> i32 {
@@ -10,4 +11,8 @@ fn get_fibonnaci_value_by_index(i : i32) -> i32 {
         return 1;
     }
     return get_fibonnaci_value_by_index(i - 1) + get_fibonnaci_value_by_index(i - 2);
+}
+
+fn get_is_even(i : i32) -> bool {
+    return 2%i == 0;
 }
